456e6d23bfc6e3d231e17dfe0ac33738e7b44c0e
OctoPrima <98483991+OctoPrima@users.noreply.github.com>
2024-10-20 23:30:30 +0300

    new footer & add mobil version header

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,12 @@
+  const btnburger = document.querySelector('.btn-burger') 
+  const mobileContainer = document.querySelector('.mobail-container')
+
+  console.log(mobileContainer);
   
+
+  const toggleMobileMenu =()=> {
+    mobileContainer.classList.toggle('show')
+    btnburger.classList.toggle('active')
+  }
+
+  btnburger.addEventListener('click', toggleMobileMenu)
