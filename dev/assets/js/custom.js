0be40041200ffa404f43683f625ff232ae908034
OctoPrima <98483991+OctoPrima@users.noreply.github.com>
2024-11-02 22:25:36 +0200

    banner & mobile ver.

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -10,3 +10,13 @@
   }
 
   btnburger.addEventListener('click', toggleMobileMenu)
+
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
